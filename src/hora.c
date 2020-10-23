48e2ff5399fd3a73f4e73d646dbba3b46b341423
Nicolás Veas <71232882+NicolasVeas@users.noreply.github.com>
2020-10-23 18:58:25 -0300

    Add files via upload

--- src/hora.c
+++ src/hora.c
@@ -13,19 +13,18 @@ int main(int argc, char* argv[]){
 
 	// "my_handler" es el manejador de señales, es decir en el controlamos la señal
 	
+	signal(SIGINT,my_handler); //esta señal esta en espera de la señal que se realiza con ctrl+c
+		
+	//Imprime que el proceso actual y que esta listo para recibir una señal
+	printf("Programa hora ejecutandose. PID=%d.\n",getpid());
+	printf("Listo para recibir la señal SIGUSR1.\n"); 
 	
-	printf("Programa hora ejecutandose. PID=%d.\n",getpid()); //imprime el proceso actual
-	printf("Listo para recibir la señal SIGUSR1.\n"); //imprime que estamos listo para recibir una señal
-	signal(SIGINT,my_handler);
-		
+
 	for(int i=1;;i++){ //bucle infinito con un sleep de 1 segundo para evitar consumir recursos y esperar una señal
-		signal(SIGUSR1, my_handler); //registra el manejador de señales --> SIGUSR1
-		
-		
-		
+		signal(SIGUSR1, my_handler); //registra el manejador de señales --> SIGUSR1		
 		sleep(1); 
 	}
-	
+
 	return 0;
 }
 
@@ -36,11 +35,11 @@ void my_handler(int senal){ //en esta funcion es para controlar las señales y s
 	time_t rawtime;
 	time(&rawtime);
 			
-
-	if(senal == SIGUSR1){ //si llega una señal de tipo sigusr1, imprime la hora actual del sistema
+	//si llega una señal de tipo sigusr1, imprime la hora actual del sistema
+	if(senal == SIGUSR1){ 
 		printf("Señal SIGUSR1 recibida: %s \n",ctime (&rawtime));
-		printf("Listo para recibir la señal SIGUSR1.\n"); //imprime que estamos listo para recibir una señal
-	}else if(senal == SIGINT){
+		printf("Listo para recibir la señal SIGUSR1.\n"); //imprime  nuevamente despues de recibir una señal que estamos listo para recibir una señal
+	}else if(senal == SIGINT){ //si se recibe una señal de tipo ctr+c se debe presionar dos veces la combinacion para salir del proceso
 		signal(SIGINT,SIG_DFL);	
 	}
 	
