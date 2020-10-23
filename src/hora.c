c510ea4dc861f18dfb055324e4ab924c7e2fbfd4
Nicolás Veas <71232882+NicolasVeas@users.noreply.github.com>
2020-10-23 18:47:44 -0300

    Add files via upload

--- src/hora.c
+++ src/hora.c
@@ -9,21 +9,20 @@
 
 void my_handler(int senal);
 
-
 int main(int argc, char* argv[]){
 
 	// "my_handler" es el manejador de señales, es decir en el controlamos la señal
 	
 	
-	//signal recibe por parametro el numero de señal y la funcion de control.
-	
-	signal(SIGUSR1, my_handler); //registra el manejador de señales --> SIGUSR1
-	signal(SIGINT,my_handler); //registra el manejador de señales --> SIGINT
-	
 	printf("Programa hora ejecutandose. PID=%d.\n",getpid()); //imprime el proceso actual
 	printf("Listo para recibir la señal SIGUSR1.\n"); //imprime que estamos listo para recibir una señal
-	
+	signal(SIGINT,my_handler);
+		
 	for(int i=1;;i++){ //bucle infinito con un sleep de 1 segundo para evitar consumir recursos y esperar una señal
+		signal(SIGUSR1, my_handler); //registra el manejador de señales --> SIGUSR1
+		
+		
+		
 		sleep(1); 
 	}
 	
@@ -36,13 +35,16 @@ void my_handler(int senal){ //en esta funcion es para controlar las señales y s
 	//variables para obtener la hora del sistema
 	time_t rawtime;
 	time(&rawtime);
-	
+			
+
 	if(senal == SIGUSR1){ //si llega una señal de tipo sigusr1, imprime la hora actual del sistema
 		printf("Señal SIGUSR1 recibida: %s \n",ctime (&rawtime));
-	}else if(senal == SIGINT){ //para salir se debe presionar dos veces ctrl+c llamando nuevamente a SIGINT
-		signal(SIGINT,SIG_DFL);
+		printf("Listo para recibir la señal SIGUSR1.\n"); //imprime que estamos listo para recibir una señal
+	}else if(senal == SIGINT){
+		signal(SIGINT,SIG_DFL);	
 	}
 	
 }
+
 
 
